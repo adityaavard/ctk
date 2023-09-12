a3b8c1304695c203399222a70464870a7babd775
aditya <adityagamar534@gmail.com>
2023-09-12 09:49:42 +0530

    revived tk

---
+++ ctk.py
@@ -0,0 +1,17 @@
+
+import customtkinter
+from tkinter import *
+#custom color theme
+customtkinter.set_appearance_mode("system")
+customtkinter.set_default_color_theme("dark-blue")
+
+#main window
+ctk = customtkinter.CTk()
+ctk.geometry("720x480")
+ctk.title("window")
+
+#mainloop
+ctk.mainloop()
+
+#print failure message
+print("faliure")
